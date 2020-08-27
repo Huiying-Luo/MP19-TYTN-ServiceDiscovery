746f997f1319495ec64ce2f3621d3d37f5156b25
Laverne <hluo0014@student.monash.edu>
2020-08-27 17:04:24 +1000

    Initial Commit

---
+++ app/src/main/java/com/laverne/servicediscover/MainActivity.java
@@ -0,0 +1,14 @@
+package com.laverne.servicediscover;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
