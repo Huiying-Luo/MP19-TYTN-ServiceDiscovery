ae1fbb0f73ab1baf6fd056d53b4d08d9e78ee161
Laverne <hluo0014@student.monash.edu>
2020-08-28 18:05:27 +1000

    Initial Commit

---
+++ app/src/main/java/com/laverne/servicediscover/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.laverne.servicediscover;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
